70bb2edd31055f0778617376a6f820b7d8e3d5a5
Huỳnh Nguyên Phúc <phucthcsmyloc@gmail.com>
2025-05-20 18:41:55 +0700

    New version

--- 00_src/adder.v
+++ 00_src/adder.v
@@ -1,31 +1,37 @@
+// Module: adder
 module adder (
-    input  wire [3:0] a,
-    input  wire [3:0] b,
-    input  wire       c_in,
-    output wire [3:0] s_out,
-    output wire       c_out
+    input  wire [3:0] A,
+    input  wire [3:0] B,
+    input  wire       CI,
+    output wire [3:0] S,
+    output wire       CO
 );
-    wire [3:0] carry;
+    // Internal carry chain
+    wire [3:0] C;
 
+    // LSB full adder
+    full_adder fa0_inst (
+        .a(A[0]),
+        .b(B[0]),
+        .c_in(CI),
+        .s_out(S[0]),
+        .c_out(C[0])
+    );
+
+    // Remaining full adders
+    genvar i;
     generate
-        genvar i;
-        full_adder fa0 (
-            .a(a[0]),
-            .b(b[0]),
-            .c_in(c_in),
-            .s_out(s_out[0]),
-            .c_out(carry[0])
-        );
-        for (i = 1; i < 4; i = i + 1) begin
-            full_adder fa (
-                .a(a[i]),
-                .b(b[i]),
-                .c_in(carry[i-1]),
-                .s_out(s_out[i]),
-                .c_out(carry[i])
+        for (i = 1; i < 4; i = i + 1) begin : fa_chain
+            full_adder fa_inst (
+                .a(A[i]),
+                .b(B[i]),
+                .c_in(C[i-1]),
+                .s_out(S[i]),
+                .c_out(C[i])
             );
         end
     endgenerate
 
-    assign c_out = carry[3];
+    // Final carry output
+    assign CO = C[3];
 endmodule
---
+++ genus.cmd
@@ -0,0 +1,7 @@
+# Cadence Genus(TM) Synthesis Solution, Version GENUS15.20 - 15.20-p004_1, built Nov 14 2015
+
+# Date: Mon May 19 01:52:59 2025
+# Host: centos7 (x86_64 w/Linux 3.10.0-1160.108.1.el7.x86_64) (2*AMD Ryzen 5 6600H with Radeon Graphics 512KB)
+# OS:   CentOS Linux release 7.9.2009 (Core)
+
+source ./03_synth/genus_shell.tcl
